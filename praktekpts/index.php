34cde5ecb5f8187337c61203b96840ad1af9cf67
AkbarSiddiq <73871902+AkbarSiddiq@users.noreply.github.com>
2021-10-03 01:44:23 +0700

    Add files via upload

---
+++ praktekpts/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title>PTSAKBAR</title>
+</head>
+<body>
+  <!-- header -->
+        <nav class="navbar-expand-lg bg-dark navbar-dark">
+            <div class="container">
+                <a href="#" class="navbar-brand" style="float:left">AkbarSiddiq</a>
+                
+                <button 
+                class="navbar-toggler" 
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#navmenu">
+                <span class="navbar-toggler-icon" ></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navmenu">
+                    <ul class="navbar-nav ms-auto" style=>
+                        <li class="nav-item">
+                            <a href="#portofolio" class="nav-link">My Portofolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#contact" class="nav-link">Contact Me Here</a>
+                        </li>
+                    </ul>
+                </div>
+                    
+                </div>
+            
+            </div>
+        </nav>
+
+        <section class="bg-dark text-light p-5 text-center" id="btt">
+            <div class="container">
+                <div class="d-flex">
+                    <div style="text-align:justify">
+                        <h2>Hello Everyone</h2>
+                        <p>My name is <span class="text-warning">Muhammad Rizky Akbar Siddiq</span> Nice to known you all</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </section>
+
+    <!-- main -->
+
+    <section id="portofolio" class="p-5">
+      <div class="container">
+        <div class="row align-items-center justify-content-between">
+          <div class="col-md">
+            <img src="tokopedia.jpg" class="img-fluid" alt="" />
+          </div>
+          <div class="col-md p-5">
+            <h2>Desain Tokopedia Web</h2>
+            <p class="lead">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Similique deleniti possimus magnam corporis ratione facere!
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque
+              reiciendis eius autem eveniet mollitia, at asperiores suscipit
+              quae similique laboriosam iste minus placeat odit velit quos,
+              nulla architecto amet voluptates?
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="learn" class="p-5 bg-dark text-light">
+      <div class="container">
+        <div class="row align-items-center justify-content-between">
+          <div class="col-md p-5">
+            <h2>Bank Jago Web Developer</h2>
+            <p class="lead">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Similique deleniti possimus magnam corporis ratione facere!
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque
+              reiciendis eius autem eveniet mollitia, at asperiores suscipit
+              quae similique laboriosam iste minus placeat odit velit quos,
+              nulla architecto amet voluptates?
+            </p>
+          </div>
+          <div class="col-md">
+            <img src="bankjago.jpg" class="img-fluid" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="p-5" id="contact">
+      <div class="container">
+        <div class="row g-4">
+          <div class="col-md">
+            <h2 class="text-center mb-4">Contact Info</h2>
+            <ul class="list-group list-group-flush lead">
+              <li class="list-group-item">
+                <span class="fw-bold">Address:</span> Tajur, Kota Bogor, Jawa barat, Indonesia.
+              </li>
+              <li class="list-group-item">
+                <span class="fw-bold">Phone:</span> 0857778278831
+              </li>
+              <li class="list-group-item">
+                <span class="fw-bold">Email:</span> akbarsiddiq402@gmail.com
+              </li>
+              <li class="list-group-item">
+                <span class="fw-bold">School:</span> SMKN 4 BOGOR
+              </li>
+            </ul>
+          </div>
+    </section>
+
+    <!-- Footer -->
+
+    <footer class="p-5 bg-dark text-white text-center position-relative">
+      <div class="container">
+        <p class="lead">Copyright &copy; 2021 by Planet M.R.A.S</p>
+      </div>
+    </footer>
+
+    
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+</html>
